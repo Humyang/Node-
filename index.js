108c603bfe81e374f8976c28e602f736ec2b6d66
Yangxg <jessebutchman@gmail.com>
2016-10-18 11:06:29 +0800

    update

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ var router = require('koa-router')()
 var serve = require('koa-static');
 
 var fs = require('fs');
-
+var moveFile = function(oldPath,newPath) {
+  return function(fn) {
+    fs.rename(oldPath, newPath,fn)
+  }
+}
+router.options('/upload', function*(next){
+  this.body=true
+})
 router.post('/upload', body(), function * (next) {
   // console.log(this.request.files[0].path)
   // console.log(this.request.fields)
@@ -15,23 +22,28 @@ router.post('/upload', body(), function * (next) {
   // there's no `.body` when `multipart`,
   // `urlencoded` or `json` request
   // console.log(this.request.body)
-
-
-    fs.rename(this.request.files[0].path, './upload/ss.png', 
-        function (err) {
-            console.log(err)
-          if (err) {
-              if (err.code === 'EXDEV') {
-                  copy_and_delete();
-              } else {
-                  callback(err);
-              }
-              return;// << both cases (err/copy_and_delete)
-          }
-          // yield next
-          // callback();
-        }
-    );
+    // console.log(this.request.files[0])
+    var file = this.request.files[0]
+    var obj = yield moveFile(file.path, './upload/'+file.name)
+    this.body = {
+      status:1,
+      img_url:"http://localhost:3000/upload/"+file.name
+    }
+    // yield fs.rename(this.request.files[0].path, './upload/ss.png', 
+    //     function (err) {
+    //         console.log(err)
+    //       if (err) {
+    //           if (err.code === 'EXDEV') {
+    //               copy_and_delete();
+    //           } else {
+    //               callback(err);
+    //           }
+    //           return;// << both cases (err/copy_and_delete)
+    //       }
+    //       // yield next
+    //       // callback();
+    //     }
+    // );
   // print it to the API requester
   // this.body = JSON.stringify({
   //   fields: this.request.fields,
@@ -43,7 +55,7 @@ router.post('/upload', body(), function * (next) {
 })
 app.use(serve('.'));
 app.use(router.routes())
-app.listen(4292)
+app.listen(3000)
 
 var format = require('util').format
 var host = 'http://localhost:4292'
