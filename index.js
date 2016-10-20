f620a959ca16873e405d248c6aaf3f4c5ebe5116
Yangxg <jessebutchman@gmail.com>
2016-10-20 18:02:42 +0800

    update

--- index.js
+++ index.js
@@ -16,42 +16,15 @@ router.options('/upload', function*(next){
   this.body=true
 })
 router.post('/upload', body(), function * (next) {
-  // console.log(this.request.files[0].path)
-  // console.log(this.request.fields)
-  // console.log(this.request.files.path)
-  // there's no `.body` when `multipart`,
-  // `urlencoded` or `json` request
-  // console.log(this.request.body)
-    // console.log(this.request.files[0])
+
     var file = this.request.files[0]
     var obj = yield moveFile(file.path, './upload/'+file.name)
     this.body = {
       status:1,
-      img_url:"http://localhost:3000/upload/"+file.name
+      img_url:"http://192.168.1.206:3000/upload/"+file.name
     }
-    // yield fs.rename(this.request.files[0].path, './upload/ss.png', 
-    //     function (err) {
-    //         console.log(err)
-    //       if (err) {
-    //           if (err.code === 'EXDEV') {
-    //               copy_and_delete();
-    //           } else {
-    //               callback(err);
-    //           }
-    //           return;// << both cases (err/copy_and_delete)
-    //       }
-    //       // yield next
-    //       // callback();
-    //     }
-    // );
-  // print it to the API requester
-  // this.body = JSON.stringify({
-  //   fields: this.request.fields,
-  //   files: this.request.files,
-  //   body: this.request.body || null
-  // }, null, 2)
-
-  // yield next
+    console.log(this.body)
+    
 })
 app.use(serve('.'));
 app.use(router.routes())
